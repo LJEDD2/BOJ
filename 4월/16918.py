fc46a614f1115519c0cb583de52e9683732e9a5e
Lee Jeong Eun <78216102+LJEDD2@users.noreply.github.com>
2022-04-18 14:50:57 +0900

    2022-04-18

--- 4월/16918.py
+++ 4월/16918.py
@@ -1,48 +1,44 @@
+from collections import deque
+r,c,time = map(int, input().split())
+board = [list(input()) for _ in range(r)]
+time -= 1
+dx, dy = [1,0,-1,0],[0,1,0,-1]
 
-def game_start():
-    for i in range(n):
-        for j in range(m):
-            #초기 시간 설정
-            if board[i][j] == '.':
-                board[i][j] = ['.',0]
-            else:
-                board[i][j] = ['O',2]
+def OOB(x,y):
+    return 0 <= x < r and 0 <= y < c
 
-def explode():
-    for i in range(n):
-        for j in range(m):
-            # 현재위치 포함 동서남북 폭탄 폭발
-            for d_x, d_y in dx, dy:
-                nx,ny = x + d_x, y + d_y
-
-
+def bombSet():
+    for i in range(r):
+        for j in range(c):
+            if board[i][j] != 'O':
+                board[i][j] = 'O'
 def bomb():
-    for i in range(n):
-        for j in range(m):
-            board[i][j][1] += 1
+    while queue:
+        x, y = queue.popleft()
+        board[x][y] = '.'
+        for d_x, d_y in zip(dx, dy):
+            nx = x + d_x
+            ny = y + d_y
+            if OOB(nx, ny) and board[nx][ny] == 'O':
+                board[nx][ny] = '.'
 
-            if board[i][j] == '.':
-                board[i][j][0] = 'O'
-            elif board[i][j] == 3:
-                explode()
-
-
-n,m,time = map(int, input().split())
-board = [list(input()) for _ in range(n)]
-dx, dy = [1,0,-1,0,0],[0,1,0,-1,0]
-idx = 0
-
-#초깃값
-game_start()
+def print_():
+    for i in board: print(*i, sep = "")
 
 #게임진행
-while time > 0:
+while time:
+    queue = deque()
+    for i in range(r):
+        for j in range(c):
+            if board[i][j] == 'O':
+                queue.append((i,j))
+    bombSet()
     time -= 1
 
-    if time%2 == 0:
+    if time == 0:
+        break
 
-    else:
+    bomb()
+    time -= 1
 
-#결과
-for i in board:
-    print(*i)
+print_()
