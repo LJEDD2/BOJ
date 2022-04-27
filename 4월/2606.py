b1edc5a021aa21d74963ebce68c34c90d4094326
Lee Jeong Eun <78216102+LJEDD2@users.noreply.github.com>
2022-04-27 16:21:54 +0900

    2022-04-27

--- 4월/2606.py
+++ 4월/2606.py
@@ -1,24 +1,34 @@
 from collections import deque
-n = int(input())
+n,m = map(int,input().split())
+board = [list(input()) for _ in range(n)]
+visited = [[[0]*2 for _ in range(m)]for _ in range(n)]
+dx, dy = [1,0,-1,0],[0,1,0,-1]
 
-def BFS(x):
-    queue = deque([x])
-    visited[x] = True
+def OOB(x,y):
+    return 0 <= x < n and 0 <= y < m
+
+def bfs():
+    queue = deque([(0,0,0)])
+    visited[0][0][0] = 1
 
     while queue:
-        virus = queue.popleft()
-        for i in computer[virus]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
+        x,y,cnt = queue.popleft()
+        if x == n-1 and y == m-1:
+            print(visited[x][y][cnt])
+            #for i in visited: print(*i)
+            exit()
+        for d_x,d_y in zip(dx,dy):
+            nx = x + d_x
+            ny = y + d_y
+            if OOB(nx,ny) and not visited[nx][ny][cnt]:
+                if board[nx][ny] == '0':
+                    queue.append((nx,ny,cnt))
+                    visited[nx][ny][cnt] = visited[x][y][cnt] + 1
 
-    return visited[x+1:].count(True)
+                if board[nx][ny] == '1' and cnt == 0:
+                    queue.append((nx,ny,1))
+                    visited[nx][ny][1] = visited[x][y][0] + 1
 
-computer = [[]*n for _ in range(n+1)]
-visited = [False]*(n+1)
-for _ in range(int(input())):
-    x, y = map(int, input().split())
-    computer[x].append(y)
-    computer[y].append(x)
 
-print(BFS(1))
+bfs()
+print(-1)
