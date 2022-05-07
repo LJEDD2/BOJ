e32173b12ab72ed92a50d3a24c79013cdad9379b
Lee Jeong Eun <78216102+LJEDD2@users.noreply.github.com>
2022-05-08 03:15:28 +0900

    2022-05-08

---
+++ 5월/2422_bf.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+visited = [[0]*n for _ in range(n)]
+
+for i in range(m):
+    x,y = map(int, input().split())
+    visited[x-1][y-1] = 1
+    visited[y-1][x-1] = 1
+
+for i in visited : print(*i)
+
+cnt = 0
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1, n):
+            if not visited[i][j] and not visited[i][k] and not visited[j][k]:
+                print(i+1,j+1,k+1)
+                cnt += 1
+
+
+print(cnt)
