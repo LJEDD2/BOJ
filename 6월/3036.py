a09f9755bedbf6629f0fb0a14300e62cd0d78e8d
Lee Jeong Eun <78216102+LJEDD2@users.noreply.github.com>
2022-06-07 19:45:26 +0900

    2022-06-07

---
+++ 6월/3036.py
@@ -0,0 +1,11 @@
+def gcd(x,y):
+    while y != 0:
+        x,y = y,x%y
+    return x
+
+n = int(input())
+n_list = list(map(int,input().split()))
+
+for i in n_list[1:]:
+    r = gcd(n_list[0], i)
+    print('{}/{}'.format(n_list[0]//r, i//r))
