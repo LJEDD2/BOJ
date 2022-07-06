6a044e43116ae8c94501e147699384c9d2759d82
Lee Jeong Eun <78216102+LJEDD2@users.noreply.github.com>
2022-07-06 23:46:27 +0900

    2022-07-06

---
+++ 7월/15184.py
@@ -0,0 +1,16 @@
+import string
+alpha = string.ascii_uppercase
+a_dict = {}
+for i in alpha:
+    a_dict[i] = 0
+
+s = input().upper()
+for i in s:
+    if i.isalpha():
+        if a_dict.get(i):
+            a_dict[i] += 1
+        else:
+            a_dict[i] = 1
+            
+for i in a_dict.keys():
+    print(f"{i} | "+"*"*a_dict[i])
